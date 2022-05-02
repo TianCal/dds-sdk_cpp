bbfa8deba671f36ced21effc6f00c16daa85cf06
Li Tian <carlostian927@berkeley.edu>
2022-05-02 22:52:42 +0000

    ImportUser untested

--- sample_client.cc
+++ sample_client.cc
@@ -2,9 +2,14 @@
 #include <grpc++/grpc++.h>
 #include <memory>
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
 #include <chrono>
 #include <tuple>
 #include <secp256k1.h>
+#include <openssl/sha.h>
+#include <random.h>
 using dds::CoreInfo;
 using dds::DDS;
 using dds::Empty;
@@ -86,25 +91,106 @@ private:
     std::string jwt;
 };
 
-std::tuple<int, const unsigned char *> prepare_import_user_signature(secp256k1_pubkey user_pub_key, const unsigned char *user_sec_key, secp256k1_pubkey core_pub_key, int64_t expiration_timestamp)
+std::string sha256(const std::string str)
 {
-    int64_t signature_timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Final(hash, &sha256);
+    std::stringstream ss;
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+    return ss.str();
+}
+std::tuple<int64_t, const unsigned char *> prepare_import_user_signature(secp256k1_pubkey user_pub_key, const unsigned char *user_sec_key, secp256k1_pubkey core_pub_key, int64_t expiration_timestamp)
+{
+    int64_t signature_timestamp = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
     secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
     secp256k1_ecdsa_signature sig;
-
-    unsigned char msg_hash[32];
-    secp256k1_ecdsa_sign(ctx, &sig, msg_hash, user_sec_key, NULL, NULL);
+    unsigned char compressed_core_pubkey[33];
+    unsigned char compressed_user_pubkey[33];
+    size_t compressed_pubkey_len;
+    compressed_pubkey_len = sizeof(compressed_core_pubkey);
+    if (!secp256k1_ec_pubkey_serialize(ctx, compressed_core_pubkey, &compressed_pubkey_len, &core_pub_key, SECP256K1_EC_COMPRESSED))
+    {   
+         throw std::invalid_argument("Invalid core public key passed");
+    }
+    compressed_pubkey_len = sizeof(compressed_user_pubkey);
+    if (!secp256k1_ec_pubkey_serialize(ctx, compressed_user_pubkey, &compressed_pubkey_len, &user_pub_key, SECP256K1_EC_COMPRESSED))
+    {   
+         throw std::invalid_argument("Invalid core public key passed");
+    }
+    unsigned char signature_time_stamp_bytes[sizeof(signature_timestamp)];
+    std::copy(static_cast<const unsigned char*>(static_cast<const void*>(&signature_timestamp)),
+          static_cast<const unsigned char*>(static_cast<const void*>(&signature_timestamp)) + sizeof signature_timestamp,
+          signature_time_stamp_bytes);
+    unsigned char expiration_time_stamp_bytes[sizeof(expiration_timestamp)];
+    std::copy(static_cast<const unsigned char*>(static_cast<const void*>(&expiration_timestamp)),
+          static_cast<const unsigned char*>(static_cast<const void*>(&expiration_timestamp)) + sizeof expiration_timestamp,
+          expiration_time_stamp_bytes); 
+    unsigned char msg[sizeof(expiration_timestamp) + sizeof(signature_timestamp) + sizeof(compressed_core_pubkey) + sizeof(compressed_user_pubkey)];
+    memcpy(msg, compressed_user_pubkey, sizeof(compressed_user_pubkey));
+    memcpy(msg + sizeof(compressed_user_pubkey), signature_time_stamp_bytes, sizeof(signature_time_stamp_bytes));
+    memcpy(msg + sizeof(compressed_user_pubkey) + sizeof(signature_time_stamp_bytes), expiration_time_stamp_bytes, sizeof(expiration_time_stamp_bytes));
+    memcpy(msg + sizeof(compressed_user_pubkey) + sizeof(signature_time_stamp_bytes) + sizeof(expiration_time_stamp_bytes), compressed_core_pubkey, sizeof(compressed_core_pubkey));
+    std::string msg_str(reinterpret_cast<char*>(msg));
+    const unsigned char* msg_hash = reinterpret_cast<unsigned char*>(const_cast<char*>(sha256(msg_str).c_str()));
+    if (!secp256k1_ecdsa_sign(ctx, &sig, msg_hash, user_sec_key, NULL, NULL))
+    {
+        throw std::invalid_argument("Cannot sign the message");
+    }
+    unsigned char serialized_signature[64];
+    if (!secp256k1_ecdsa_signature_serialize_compact(ctx, serialized_signature, &sig))
+    {
+        throw std::invalid_argument("Cannot serialize signature");
+    }
+    return signature_timestamp, serialized_signature;
 }
 
-int main(int argc, char **argv)
+/*int main(int argc, char **argv)
 {
     std::string server_address{"127.0.0.1:8080"};
     // std::string jwt = argv[1];
-    std::string jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYWRtaW4iLCJ1c2VyX2lkIjoiX2FkbWluIiwiZXhwIjoxNjUxNDQyMDI1fQ.j3OuueKNw4I8LDxYoCSZIbCUarPHrdXWKOZjlQ35__Y";
+    std::string jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYWRtaW4iLCJ1c2VyX2lkIjoiX2FkbWluIiwiZXhwIjoxNjUxNzAzMTM0fQ.1KKgJhbv7AvXDCKr4B53OmQDzOsaCwPC12kw3VHNliU";
     DDSClient client{grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()), jwt};
     secp256k1_pubkey core_public_key;
     std::string core_mq_uri;
     std::tie(core_mq_uri, core_public_key) = client.request_core_info();
     std::cout << core_mq_uri << std::endl;
+    return 0;
+}*/
+
+int main(int argc, char **argv)
+{
+    std::string server_address{"127.0.0.1:8080"};
+    // std::string jwt = argv[1];
+    std::string jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYWRtaW4iLCJ1c2VyX2lkIjoiX2FkbWluIiwiZXhwIjoxNjUxNzAzMTM0fQ.1KKgJhbv7AvXDCKr4B53OmQDzOsaCwPC12kw3VHNliU";
+    DDSClient client{grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()), jwt};
+    int64_t expiration_timestamp = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count() + 86400 *31;
+    secp256k1_pubkey core_public_key;
+    std::string core_mq_uri;
+    std::tie(core_mq_uri, core_public_key) = client.request_core_info();
+
+    unsigned char seckey[32];
+    secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
+    while (1) {
+        if (!fill_random(seckey, sizeof(seckey))) {
+            printf("Failed to generate randomness\n");
+            return 1;
+        }
+        if (secp256k1_ec_seckey_verify(ctx, seckey)) {
+            break;
+        }
+    }
+    secp256k1_pubkey user_pubkey;
+    int return_val = secp256k1_ec_pubkey_create(ctx, &user_pubkey, seckey);
+    assert(return_val);
+    std::int64_t signature_timestamp;
+    const unsigned char *serialized_signature;
+    std::tie(signature_timestamp, serialized_signature) = prepare_import_user_signature(user_pubkey, seckey, expiration_timestamp);
+    std::cout << client.import_user(core_public_key, signature_timestamp, expiration_timestamp, serialized_signature);
     return 0;
 }
