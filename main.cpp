127fe557b7950e37ad028869fd1f0a3401f12e7a
Li Tian <carlostian927@berkeley.edu>
2022-05-03 04:22:32 +0000

    Gen user with segfault

--- main.cpp
+++ main.cpp
@@ -3,3 +3,48 @@
 int main(int, char**) {
     std::cout << "Hello, world!\n";
 }
+/*int main(int argc, char **argv)
+{
+    std::string server_address{"127.0.0.1:8080"};
+    // std::string jwt = argv[1];
+    std::string jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYWRtaW4iLCJ1c2VyX2lkIjoiX2FkbWluIiwiZXhwIjoxNjUxNzAzMTM0fQ.1KKgJhbv7AvXDCKr4B53OmQDzOsaCwPC12kw3VHNliU";
+    DDSClient client{grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()), jwt};
+    secp256k1_pubkey core_public_key;
+    std::string core_mq_uri;
+    std::tie(core_mq_uri, core_public_key) = client.request_core_info();
+    std::cout << core_mq_uri << std::endl;
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    std::string server_address{"127.0.0.1:8027"};
+    // std::string jwt = argv[1];
+    std::string jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYWRtaW4iLCJ1c2VyX2lkIjoiX2FkbWluIiwiZXhwIjoxNjUxNzE1Njg3fQ.pakDhL__f6LTqM2cLna5E0Wsv7sFzU_UG8VgY_Z1UPI";
+    DDSClient client{grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()), jwt};
+    //int64_t expiration_timestamp = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count() + 86400 *31;
+    int64_t expiration_timestamp = 1651537665 + 86400 *31;
+    secp256k1_pubkey core_public_key;
+    std::string core_mq_uri;
+    std::tie(core_mq_uri, core_public_key) = client.request_core_info();
+
+    unsigned char seckey[32];
+    secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
+    while (1) {
+        if (!fill_random(seckey, sizeof(seckey))) {
+            printf("Failed to generate randomness\n");
+            return 1;
+        }
+        if (secp256k1_ec_seckey_verify(ctx, seckey)) {
+            break;
+        }
+    }
+    secp256k1_pubkey user_pubkey;
+    int return_val = secp256k1_ec_pubkey_create(ctx, &user_pubkey, seckey);
+    assert(return_val);
+    std::int64_t signature_timestamp;
+    const unsigned char *serialized_signature;
+    std::tie(signature_timestamp, serialized_signature) = prepare_import_user_signature(user_pubkey, seckey, core_public_key, expiration_timestamp);
+    std::cout << client.import_user(user_pubkey, signature_timestamp, expiration_timestamp, serialized_signature);
+    return 0;
+}*/
--- sample_client.cc
+++ sample_client.cc
@@ -54,7 +54,7 @@ public:
         }
         else
         {
-            return "bad";
+            //return "bad";
             throw std::invalid_argument("RPC failed" + status.error_code() + std::string(":") + status.error_message());
         }
     }
@@ -138,19 +138,20 @@ std::tuple<int64_t, const unsigned char *> prepare_import_user_signature(secp256
     secp256k1_ecdsa_signature_serialize_compact(ctx, serialized_signature, &sig);
     return std::make_tuple(signature_timestamp, serialized_signature);
 }
-
-/*int main(int argc, char **argv)
+void generate_user(secp256k1_pubkey user_public_key, unsigned char* seckey) 
 {
-    std::string server_address{"127.0.0.1:8080"};
-    // std::string jwt = argv[1];
-    std::string jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYWRtaW4iLCJ1c2VyX2lkIjoiX2FkbWluIiwiZXhwIjoxNjUxNzAzMTM0fQ.1KKgJhbv7AvXDCKr4B53OmQDzOsaCwPC12kw3VHNliU";
-    DDSClient client{grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()), jwt};
-    secp256k1_pubkey core_public_key;
-    std::string core_mq_uri;
-    std::tie(core_mq_uri, core_public_key) = client.request_core_info();
-    std::cout << core_mq_uri << std::endl;
-    return 0;
-}*/
+    secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
+    while (1) {
+        if (!fill_random(seckey, 32)) {
+            throw std::invalid_argument("Failed to generate randomness\n");
+        }
+        if (secp256k1_ec_seckey_verify(ctx, seckey)) {
+            break;
+        }
+    }
+    if (!secp256k1_ec_pubkey_create(ctx, &user_public_key, seckey))
+        throw std::invalid_argument("Cannot create publickey");
+}
 
 int main(int argc, char **argv)
 {
@@ -165,22 +166,22 @@ int main(int argc, char **argv)
     std::tie(core_mq_uri, core_public_key) = client.request_core_info();
 
     unsigned char seckey[32];
+    secp256k1_pubkey user_public_key;
+    //generate_user(user_public_key, seckey);
     secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
     while (1) {
-        if (!fill_random(seckey, sizeof(seckey))) {
-            printf("Failed to generate randomness\n");
-            return 1;
+        if (!fill_random(seckey, 32)) {
+            throw std::invalid_argument("Failed to generate randomness\n");
         }
         if (secp256k1_ec_seckey_verify(ctx, seckey)) {
             break;
         }
     }
-    secp256k1_pubkey user_pubkey;
-    int return_val = secp256k1_ec_pubkey_create(ctx, &user_pubkey, seckey);
-    assert(return_val);
+    if (!secp256k1_ec_pubkey_create(ctx, &user_public_key, seckey))
+        throw std::invalid_argument("Cannot create publickey");
     std::int64_t signature_timestamp;
     const unsigned char *serialized_signature;
-    std::tie(signature_timestamp, serialized_signature) = prepare_import_user_signature(user_pubkey, seckey, core_public_key, expiration_timestamp);
-    std::cout << client.import_user(user_pubkey, signature_timestamp, expiration_timestamp, serialized_signature);
+    std::tie(signature_timestamp, serialized_signature) = prepare_import_user_signature(user_public_key, seckey, core_public_key, expiration_timestamp);
+    std::cout << client.import_user(user_public_key, signature_timestamp, expiration_timestamp, serialized_signature) << std::endl;
     return 0;
 }
