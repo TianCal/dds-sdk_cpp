1a542c623d22df9a704267a2e7f965e1ad448b02
Li Tian <carlostian927@berkeley.edu>
2022-05-09 17:09:15 -0700

    confirm_task, refresh_token

---
+++ examples/user_refresh_token.cc
@@ -0,0 +1,14 @@
+#include <secp256k1.h>
+#include <grpc++/grpc++.h>
+#include "../sample_client.h"
+
+int main(int argc, char **argv)
+{
+    using std::string;
+    using namespace dds;
+    string server_address = argv[1];
+    string jwt = argv[2];
+    DDSClient client{grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()), jwt};
+    string new_jwt = client.refresh_token();
+    std::cout << "New JWT: " << new_jwt << std::endl;
+}
