e6657fe7e9e22e249c867e7104d9edde2a44a57c
Li Tian <carlostian927@berkeley.edu>
2022-05-09 18:13:27 -0700

    get_net and import_guest_jwt done

---
+++ examples/get_next_greeting_message.cc
@@ -0,0 +1,31 @@
+#include <secp256k1.h>
+#include <grpc++/grpc++.h>
+#include "../sample_client.h"
+#include <chrono>
+int main(int argc, char **argv)
+{
+    using std::string;
+    using namespace dds;
+    string server_address = argv[1];
+    string jwt = argv[2];
+    string timestamp_str = (argc > 3) ? argv[3] : "";
+    int64_t timestamp = (argc > 3) ? strtoll(timestamp_str.c_str(), NULL, 10) : std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    DDSClient client{grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()), jwt};
+    string latest_key = "_dds_internal:protocols:greetings:finished:latest";
+    string queue_name = client.subscribe(latest_key, timestamp);
+    DdsSubscriber subscriber = client.new_subscriber(queue_name);
+    string data = subscriber.get_next();
+    SubscriptionMessage message;
+    message.ParseFromString(data); 
+    if (message.change_type() != "delete") {
+        Task task_id;
+        task_id.ParseFromString(message.payload());
+        StorageEntry read_key;
+        read_key.set_key_name("tasks:" + task_id.task_id() + ":output");
+        std::vector<StorageEntry> read_keys{read_key};
+        std::vector<StorageEntry> res = client.read_entries(read_keys);
+        StorageEntry output_entry = res[0];
+        std::cout << output_entry.payload() << std::endl;
+    }
+    return 0;
+}
---
+++ examples/user_import_guest_jwt.cc
@@ -0,0 +1,14 @@
+#include <secp256k1.h>
+#include <grpc++/grpc++.h>
+#include "../sample_client.h"
+
+int main(int argc, char **argv)
+{
+    using std::string;
+    string server_address = argv[1];
+    string jwt = argv[2];
+    string guest_jwt = argv[3];
+    DDSClient client{grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()), jwt};
+    client.import_guest_jwt(guest_jwt);
+    return 0;
+}
